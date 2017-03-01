d807f5f7bf6c30b926f1895747755f13ca1ab8ff
Terry <tet19@pitt.edu>
2017-03-01 18:02:21 -0500

    pitch/roll works in any arbitrary direction now

--- src/pitch.c
+++ src/pitch.c
@@ -15,6 +15,7 @@
 #define DEFAULTANGLE2 180.0
 #define DEFAULTANGLE3 150.0
 #define PI 3.14159265358979323846
+#define MAXTILT PI/3.0
 
 struct coordinate coord;
 
@@ -108,18 +109,6 @@ void pitch(double alpha){
 		printf("theta:%f; hypotenuse:%f\n", theta, hypotenuse);
 		printf("delta z:%f ; delta y:%f\n", delta_z, delta_y);
 
-/*  //TODO: Work on pitching from "current" positions instead
-    struct leg_status legstat0;
-    struct leg_status legstat2;
-    struct leg_status legstat3;
-    struct leg_status legstat5;
-    get_leg_status(0, &legstat0);
-    get_leg_status(2, &legstat2;
-    get_leg_status(3, &legstat3);
-    get_leg_status(5, &legstat5);
-*/
-
-
 		if(alpha < 0){
 			printf("Back pitch\n");
 		}else{
@@ -142,6 +131,73 @@ void pitch(double alpha){
 
 }
 
+void pitch_and_roll(double gamma, double magnitude){
+	printf("magnitude (degrees):%f, %f\n", magnitude, MAXTILT);
+	gamma = gamma * PI / 180.0;							//convert to radians
+	magnitude = magnitude/100.0 * MAXTILT;
+	printf("magnitude (degrees):%f, %f\n", magnitude, MAXTILT);
+	double alpha = magnitude * sin(gamma);		//alpha for pitch
+	double beta = magnitude * cos(gamma);			//beta for roll
+
+	//calculations for pitch
+	//calculating z offset and y offset
+	double theta_pitch = 0.5 * (PI - alpha);
+	double hypotenuse_pitch = 2.0 * sin((alpha/2.0)) * (BODYLENGTH/2.0);
+	double delta_z_pitch = cos((PI/2)-theta_pitch) * hypotenuse_pitch;
+	double delta_y_pitch = sin((PI/2)-theta_pitch) * hypotenuse_pitch;
+	printf("alpha (in rad):%f\n", alpha);
+	//printf("theta:%f; hypotenuse:%f\n", theta, hypotenuse);
+	printf("\tdelta z:%f ; delta y:%f\n", delta_z_pitch, delta_y_pitch);
+
+
+	//calculating z offset and x offset
+	double theta_roll = 0.5 * (PI - beta);
+	double hypotenuse_roll = 2.0 * sin((beta/2.0)) * (BODYWIDTH/2.0);
+	double delta_z_roll = cos((PI/2)-theta_roll) * hypotenuse_roll;
+	double delta_x_roll = sin((PI/2)-theta_roll) * hypotenuse_roll;
+	printf("beta (in rad):%f\n", beta);
+	//printf("theta:%f; hypotenuse:%f\n", theta, hypotenuse);
+	printf("\tdelta z:%f ; delta x:%f\n\n", delta_z_roll, delta_x_roll);
+
+	//move legs
+	//set left and right legs (only for roll)
+	//right leg
+	coord.x = START_X;
+	coord.y = START_Y + delta_x_roll;
+	coord.z = START_Z + delta_z_roll;
+	move_leg(4, &coord);
+	//left leg
+	coord.x = START_X;
+	coord.y = START_Y - delta_x_roll;
+	coord.z = START_Z - delta_z_roll;
+	move_leg(1, &coord);
+
+	//front left leg
+	coord.x = START_X + (delta_y_pitch * sin(PI/4)) - (delta_x_roll * sin(PI/4));		//= 45 degrees in radian
+	coord.y = START_Y + (delta_y_pitch * sin(PI/4)) - (delta_x_roll * sin(PI/4));
+	coord.z = START_Z + delta_z_pitch - delta_z_roll;
+	move_leg(0, &coord);
+
+	//front right leg
+	coord.x = START_X + (delta_y_pitch * sin(PI/4)) + (delta_x_roll * sin(PI/4));		//= 45 degrees in radian
+	coord.y = START_Y + (delta_y_pitch * sin(PI/4)) + (delta_x_roll * sin(PI/4));
+	coord.z = START_Z + delta_z_pitch + delta_z_roll;
+	move_leg(5, &coord);
+
+	//back left leg
+	coord.x = START_X - (delta_y_pitch * sin(PI/4)) - (delta_x_roll * sin(PI/4));
+	coord.y = START_Y - (delta_y_pitch * sin(PI/4)) - (delta_x_roll * sin(PI/4));
+	coord.z = START_Z - delta_z_pitch - delta_z_roll;
+	move_leg(2, &coord);
+
+	//back right left
+	coord.x = START_X - (delta_y_pitch * sin(PI/4)) + (delta_x_roll * sin(PI/4));
+	coord.y = START_Y - (delta_y_pitch * sin(PI/4)) + (delta_x_roll * sin(PI/4));
+	coord.z = START_Z - delta_z_pitch + delta_z_roll;
+	move_leg(3, &coord);
+
+}
+
 int main(void)
 {
     char portName[] = "/dev/ttyUSB0";
@@ -152,7 +208,7 @@ int main(void)
     openPort(portName);
 		resetAllLegs();
 
-		printf("Select mode:\n\t1. Pitch\n\t2. Roll\n");
+		printf("Select mode:\n\t1. Pitch\n\t2. Roll\n\t3. Arbitrary direction\n");
 		scanf(" %s", buffer);
 		int choice = atoi(buffer);
 
@@ -170,6 +226,16 @@ int main(void)
         double alpha = atoi(buffer);
 				if(choice == 1)     pitch(alpha);
 				if(choice == 2)	    roll(alpha);
+				if(choice == 3){
+					printf("Select magnitude (1-100): ");
+					scanf(" %s", buffer);
+					if(buffer[0] == 'q'){
+						break;
+					}
+					double mag = atoi(buffer);
+					printf("angle + magnitude you entered: %f , %f\n", alpha, mag);
+					pitch_and_roll(alpha, mag);
+				}
     }
 
     closePort(portName);
